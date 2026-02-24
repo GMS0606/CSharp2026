bf7940fef9b983ed79927b2d3ac52a5cca6a3b0d
GMS0606 <geovanamoreiradossantos639@gmail.com>
2026-02-23 21:44:36 -0300

    aula06

--- Logica de Programacao e Algoritmos/Aula06/exercicio.cs
+++ Logica de Programacao e Algoritmos/Aula06/exercicio.cs
@@ -21,23 +21,20 @@ bool valor3 = b > c;
 bool valor4 = a <= c;
 
 WriteLine($"O primeiro valor é igual ao tercerio valor? {valor1}");
-WriteLine();
+
 WriteLine($"O segundo valor é diferente do primeiro valor? {valor2}");
-WriteLine();
+
 WriteLine($"O primeiro valor é menor ou igual o terceiro valor? {valor4}");
+
 WriteLine();
 
 bool per1 = valor1 & valor3;
 
 WriteLine($"O resultado da comparação entre a pergunta 1 e a pergunta 3 serem verdadeiros é: {per1}");
 
-WriteLine();
-
 bool per2 = valor2 | valor4;
 
 WriteLine($"O resultado da comparação entre a pergunta 2 e a pergunta 4 referente a pelo menos uma ser verdadeira é: {per2}");
-
-WriteLine();
 
 bool per3 = !valor1;
 
