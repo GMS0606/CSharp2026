2f280f00c202efa46deed31ca0f876b6fa29731b
GMS0606 <geovanamoreiradossantos639@gmail.com>
2026-02-23 21:42:02 -0300

    aula6

--- Logica de Programacao e Algoritmos/Aula06/exercicio.cs
+++ Logica de Programacao e Algoritmos/Aula06/exercicio.cs
@@ -13,23 +13,31 @@ b = int.Parse(ReadLine());
 Write ("Digite o valor de C: ");
 c = int.Parse(ReadLine());
 
+WriteLine();
+
 bool valor1 = a == c;
 bool valor2 = b != a; 
 bool valor3 = b > c;
 bool valor4 = a <= c;
 
 WriteLine($"O primeiro valor é igual ao tercerio valor? {valor1}");
+WriteLine();
 WriteLine($"O segundo valor é diferente do primeiro valor? {valor2}");
-WriteLine($"O segundo valor é maior que o terceiro valor? {valor3}");
+WriteLine();
 WriteLine($"O primeiro valor é menor ou igual o terceiro valor? {valor4}");
+WriteLine();
 
 bool per1 = valor1 & valor3;
 
 WriteLine($"O resultado da comparação entre a pergunta 1 e a pergunta 3 serem verdadeiros é: {per1}");
 
+WriteLine();
+
 bool per2 = valor2 | valor4;
 
 WriteLine($"O resultado da comparação entre a pergunta 2 e a pergunta 4 referente a pelo menos uma ser verdadeira é: {per2}");
+
+WriteLine();
 
 bool per3 = !valor1;
 
